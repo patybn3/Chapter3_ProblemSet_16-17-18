db4ff45a3d2ad1f9b6a5c8607f590eb8d4239542
patybn3 <paty.nigro@gmail.com>
2022-10-07 11:22:31 -0400

    Insert into list, check even numbers, check total of even numbers, check if list contains a certain number, remove that number.

--- src/ProblemSet_3_PatriciaAntlitz.java
+++ src/ProblemSet_3_PatriciaAntlitz.java
@@ -3,8 +3,22 @@ public class ProblemSet_3_PatriciaAntlitz
     public static void main(String[] args)
     {
         LinkedList newList = new LinkedList();
+        int random = (int) (Math.random() * (20 - 1)) + 1;
+
         newList.generateNums();
+        System.out.println("\nEven Numbers:");
         newList.numEvens(newList.listLink);
-        //newList.display();
+        System.out.println("\nTotal Even Numbers: " + newList.printEvens(newList.listLink));
+        System.out.println("\nTarget = " + random);
+
+        if(newList.contains(random, newList.listLink)){
+            System.out.println("Target Found!");
+            newList.contains(random, newList.listLink);
+        }
+        else{
+            System.out.println("Target Not Found!");
+        }
+        System.out.println("Final List: ");
+        newList.remove(random, newList.listLink);
     }
 }

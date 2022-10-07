c25ea0e3838a7e5cf49bab1a0a03cb474f15fd61
patybn3 <paty.nigro@gmail.com>
2022-10-07 11:51:39 -0400

    comments

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -2,16 +2,20 @@ import java.util.Arrays;
 
 public class LinkedList<T>
 {
+    //node list
     protected  LLNode<Integer> listLink;
+    //I prefer to pass variables as global
     int range;
     int random;
     int count;
 
+    //initiate node list
     public LinkedList()
     {
         listLink = null;
     }
 
+    //method used to insert each number generate into the node list
     public void generateList(int num)
     {
         LLNode<Integer> holdList = new LLNode<>(num);
@@ -19,46 +23,50 @@ public class LinkedList<T>
         listLink = holdList;
     }
 
+    //generate the 10 random numbers
     public void generateNums()
     {
+        //the range will be between "1-20"
         range = (20 - 1) + 1;
 
         for(int i = 1; i < 10; i++)
         {
             random = (int) (Math.random() * range) + 1;
+            //calls the method that inserts, passing each individual random number
             generateList(random);
             System.out.println("num = " + listLink.getInfo());
         }
     }
-
-    public int printEvens(LLNode<Integer> list){
-        count = 0;
-
+    //this method checks how many even numbers the list has
+    public int numEvens(LLNode<Integer> list)
+    {
         if(list == null)
         {
             return 0;
         }
-        else if(list.info % 2 == 0)
+        else if(list.info % 2 == 0) //if the number evenly divides by 2 (no remainder)
         {
             count = 1;
+            //will return the total count
             return count + printEvens(list.getLink());
         }
         else
         {
+            //if not, it won't count
             return printEvens(list.getLink());
         }
     }
-
-    public int numEvens(LLNode<Integer> list)
+    //printEvens is not a required, but its here in order to test the total number of even numbers given by the previous
+    //method (without having to manually count it).
+    public int printEvens(LLNode<Integer> list)
     {
-        count = 0;
-
         if(list == null)
         {
             return 0;
         }
-        else if(list.info % 2 == 0)
+        else if(list.info % 2 == 0) //if the number evenly divides by 2 (no remainder)
         {
+            //this will print each number that is even
             System.out.print(list.getInfo() + " ");
             count = 1;
             return count + numEvens(list.getLink());
@@ -68,14 +76,14 @@ public class LinkedList<T>
             return numEvens(list.getLink());
         }
     }
-
+    //target is a parameter passed in the main method as a random number
     public boolean contains(int target, LLNode<Integer> list)
     {
         if(list == null)
         {
             return false;
         }
-        else if(list.info == target)
+        else if(list.info == target) //if the target given is present in the list
         {
             return true;
         }
@@ -85,15 +93,19 @@ public class LinkedList<T>
     }
 
     public void remove(int target, LLNode<Integer> list) {
-        if (list == null) //If the list is null
-            return; //Do nothing.
-        else if (list.getInfo() == target) //If the first node value is target.
+        if(list == null)
         {
-            list = list.getLink(); //Bypass the first node.
-            remove(target, list); //Call the remove() for remaining nodes.
-            System.out.println("- Target " + target + " Removed!");
+            return;
+        }
+        else if(list.getInfo() == target) //if the target is present in the lsit
+        {
+            list = list.getLink();
+            remove(target, list);
+            //prints the numbers removed
+            System.out.println(" -> # " + target + " Removed ");
         }
         else {
+            //prints the final list
             System.out.print(list.getInfo() + " ");
             remove(target, list.getLink());
         }
